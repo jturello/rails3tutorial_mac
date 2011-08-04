c2bcc27070a3a3777f870342b7da08d57413b976
jturello <jdturello@gmail.com>
2011-08-04 05:01:09 -0500

    css fix td.class=profile

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,6 +3,6 @@
 Factory.define :user do |user|
   user.name                   "Donald Duck"
   user.email                  "Donald@Duck.com"
-  user.password               "foobar"
-  user.password_confirmation  "foobar"
+  user.password               "quackquack"
+  user.password_confirmation  "quackquack"
 end
