ff0d182ebc413e7f261c33849bbe5fb33852d205
jturello <jdturello@gmail.com>
2011-08-10 00:16:36 -0500

    User signup complete

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,7 +44,7 @@ end
 
 Spork.each_run do
   # This code will be run each time you run your specs.
- 
+  ActiveSupport::Dependencies.clear
 end
 
 # --- Instructions ---
@@ -52,7 +52,7 @@ end
 #   code that you don't normally modify during development in the 
 #   Spork.prefork block.
 # - Place the rest under Spork.each_run block
-# - Any code that is left outside of the blocks will be ran during preforking
+# - Any code that is left outside of the blocks will be run during preforking
 #   and during each_run!
 # - These instructions should self-destruct in 10 seconds.  If they don't,
 #   feel free to delete them.
